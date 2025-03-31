6b240cc121f58bd2c5792034e035cf4fc0df18a2
Italobgr <italobrunobgr@gmail.com>
2025-03-30 23:06:53 -0300

    chore: Update routine

--- Reletorios/Lista.cs
+++ Reletorios/Lista.cs
@@ -13,4 +13,20 @@
         public string Status { get; set; }
         public DateTime? ProcessedTime { get; set; }
     }
+
+    public class StatusReport
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Status { get; set; }
+        public DateTime? ProcessedTime { get; set; }
+    }
+
+   /* public class SolicitacaoRelatorio
+    {
+        public Guid Id { get; set; }
+        public string Nome { get; set; }
+        public string Status { get; set; }
+        public DateTime? ProcessedTime { get; set; }
+    }*/
 }
